6e4e03c9408f7e7caf219036433d9a073125c4e8
Daniel Diego Wildin <ddiegow@uoc.edu>
2023-06-05 00:26:01 +0900

    requestvote args and reply structs

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -152,12 +152,18 @@ func (rf *Raft) Snapshot(index int, snapshot []byte) {
 // field names must start with capital letters!
 type RequestVoteArgs struct {
 	// Your data here (2A, 2B).
+	Term         int // candidate's term
+	CandidateId  int // candidate requesting vote
+	LastLogIndex int // index of candidate's last log entry
+	LastLogTerm  int // term of candidate's last log entry
 }
 
 // example RequestVote RPC reply structure.
 // field names must start with capital letters!
 type RequestVoteReply struct {
 	// Your data here (2A).
+	Term        int  // currentTerm, for candidate to update itself
+	VoteGranted bool // true means candidate received the vote
 }
 
 // example RequestVote RPC handler.
